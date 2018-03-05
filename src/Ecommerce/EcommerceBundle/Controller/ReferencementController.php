71cee7dcc19b236d9d7b2133b49a4ac19f698c77
mariesek <mariem.sekkat@gmail.com>
2018-03-05 15:02:53 +0000

    Update ReferencementController.php

--- src/Ecommerce/EcommerceBundle/Controller/ReferencementController.php
+++ src/Ecommerce/EcommerceBundle/Controller/ReferencementController.php
@@ -19,17 +19,17 @@ class ReferencementController extends Controller
         $session = $this->getRequest()->getSession();
         $em = $this->getDoctrine()->getManager();
 
-        if(!$session->has('referencement')) $session->set('referencement',array());
+        if (!$session->has('referencement')) $session->set('referencement', array());
 
         $villes = $em->getRepository('EcommerceBundle:ville')->findAll();
 
-       $entities = $em->getRepository('EcommerceBundle:region')->findAll();
+        $entities = $em->getRepository('EcommerceBundle:region')->findAll();
 
         return $this->render('EcommerceBundle:Default:referencement/modulesUsed/referencement.html.twig',
-          array(  'entities' => $entities,
-              'villes' => $villes,
-              'referencement'=>$session->get('referencement')
-        ));
+            array('entities' => $entities,
+                'villes' => $villes,
+                'referencement' => $session->get('referencement')
+            ));
 
 
     }
@@ -41,60 +41,101 @@ class ReferencementController extends Controller
         $em = $this->getDoctrine()->getManager();
 
 
-
         $rubriques = $em->getRepository('EcommerceBundle:Rubrique')->findAll();
         $villes = $em->getRepository('EcommerceBundle:ville')->findAll();
         $entities = $em->getRepository('EcommerceBundle:region')->findAll();
 
 
-        return $this->render('EcommerceBundle:Default:referencement/modulesUsed/referencementdevis.html.twig',  array(  'rubriques' => $rubriques,
+        return $this->render('EcommerceBundle:Default:referencement/modulesUsed/referencementdevis.html.twig', array('rubriques' => $rubriques,
             'entities' => $entities,
             'villes' => $villes,
 
-            ));
+        ));
     }
-        public function ajaxAction(Request $request)
-        {
 
-            $em = $this->getDoctrine()->getManager();
-            $prestations   =  $request->request->get('rubrique');
+    public function ajaxAction(Request $request)
+    {
 
-            $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Prestation p WHERE p.rub=:rub');
-            $query->setParameter('rub',$prestations);
-            $result = $query->getArrayResult();
+        $em = $this->getDoctrine()->getManager();
+        $prestations = $request->request->get('rubrique');
 
-         /*   $result = $em->getRepository('EcommerceBundle:Prestation')->getPrestation($prestations);*/
+        $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Prestation p WHERE p.rub=:rub');
+        $query->setParameter('rub', $prestations);
+        $result = $query->getArrayResult();
 
-
-            return new Response(json_encode($result),200);
+        /*   $result = $em->getRepository('EcommerceBundle:Prestation')->getPrestation($prestations);*/
 
 
-
-          /*  $em = $this->getDoctrine()->getManager();
-            $prestations   =  $request->request->get('rubrique');
-         /*   $rubriques = $em->getRepository('EcommerceBundle:Prestation')->findBy(array("rubrique_id"=>$prestations));*/
+        return new Response(json_encode($result), 200);
 
 
+        /*  $em = $this->getDoctrine()->getManager();
+          $prestations   =  $request->request->get('rubrique');
+       /*   $rubriques = $em->getRepository('EcommerceBundle:Prestation')->findBy(array("rubrique_id"=>$prestations));*/
 
-  /*  $response = new JsonResponse();
 
-            $prest= $this->getDoctrine()->getRepository('EcommerceBundle:Prestation')->findBy(array('rub' => $prestations ));
+        /*  $response = new JsonResponse();
 
-    */     /*   $var=array();
+                  $prest= $this->getDoctrine()->getRepository('EcommerceBundle:Prestation')->findBy(array('rub' => $prestations ));
+
+          */     /*   $var=array();
             foreach ($prest as $rub){
                  $var['id'][]=$rub->getId();
                  $var['presta'][]=$rub->getPrestation();
         }*/
 
-          /*  $prest = json_encode($prest);
+        /*  $prest = json_encode($prest);
 
-      /*  $prest= json_encode($rubriques);*/
+    /*  $prest= json_encode($rubriques);*/
 
         /*    return $response->setData(array('nom' => $prest));*/
 
-           /*$parametersAsArray = [];
-           if($content = $request ->getContent()){
-               $parametersAsArray = json_decode($content,true);
-           }*/
-        }
+        /*$parametersAsArray = [];
+        if($content = $request ->getContent()){
+            $parametersAsArray = json_decode($content,true);
+        }*/
+    }
+
+/*
+    public function ajaxRefAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $choix = array('MC','M1');
+        $query = $em->getRepository('EcommerceBundle:TarifInternet')->createQueryBuilder('t')
+            ->select('t.rGion1 AS tarif')
+            ->where('t.ctar = :code')
+            ->setParameter('code', $choix)->getQuery()
+            ->getSingleResult();
+
+
+        $response = new JsonResponse();
+        return $response->setData(array('data' => $query));
+
+
+    }*/
+
+    public function ajaxRefAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ville  =  $request->request->get('ville');
+        $opt1  =  $request->request->get('opt1');
+        $region =  $request->request->get('region');
+        $ctar =  $request->request->get('ctar');
+
+
+        $var='rGion'.$region;
+        $query = $em->getRepository('EcommerceBundle:TarifInternet')->createQueryBuilder('t')->select('t.'.$var.' AS VAR')
+            ->where('t.opt1 = :op')->setParameter('op',$opt1)
+            ->andWhere('t.ctar = :ct')->setParameter('ct',$ctar)->getQuery()->getSingleResult();
+
+
+
+
+        $response = new JsonResponse();
+        return $response->setData(array('nom' => $query));
+
+
+    }
 }
